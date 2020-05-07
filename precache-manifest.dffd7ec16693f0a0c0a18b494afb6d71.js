3a061ae47561d4a05926c5f5575f7a20592769af
William Bruntrager <w.bruntrager@gmail.com>
2020-05-07 13:36:18 +0200

    Updates

---
+++ precache-manifest.dffd7ec16693f0a0c0a18b494afb6d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfe3067f7aca893778951561a8457ee",
+    "url": "/libertalia/index.html"
+  },
+  {
+    "revision": "185658f3dbf61676e962",
+    "url": "/libertalia/static/css/main.bf46b0ea.chunk.css"
+  },
+  {
+    "revision": "c17956b3a8ba09e35161",
+    "url": "/libertalia/static/js/2.1fda4fc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/libertalia/static/js/2.1fda4fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185658f3dbf61676e962",
+    "url": "/libertalia/static/js/main.a5866ed4.chunk.js"
+  },
+  {
+    "revision": "1b1b881319a0714c2ba9",
+    "url": "/libertalia/static/js/runtime-main.b974c0b5.js"
+  }
+]);
