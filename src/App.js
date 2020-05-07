10d3eec46c5a4a3ac8edcaccde71df8379161616
William Bruntrager <w.bruntrager@gmail.com>
2020-05-07 13:33:16 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import cards from './cards.json'
 
 function App() {
+  const features = ['Rank', 'Name', 'DAY', 'DUSK', 'NIGHT', 'END OF CAMPAIGN']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <table className="striped table-responsive">
+        <tbody>
+          <tr>
+            {features.map((feature) => {
+              return <th>{feature}</th>
+            })}
+          </tr>
+          {cards.map((card) => {
+            return (
+              <tr>
+                {features.map((feature) => {
+                  return <td>{card[feature]}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
